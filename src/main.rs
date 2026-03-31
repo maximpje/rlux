926cae2166ab52d2048650da3169205fbe17a073
maximpje <wynantmaxim@gmail.com>
2026-03-31 23:01:58 +0200

    some quick changes

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ fn main() {
 
     // debug stuff
     let lex: scanner::Token = scanner::Token {
-        token_type: scanner::TokenType::String(String::from("nigga")),
+        token_type: scanner::TokenType::String(String::from("Hello, world!")),
         line: 53
     };
 
     println!("{}", scanner::Token::to_string(&lex));
 
     // debug stuff
-    println!("{}", file_reader::read_file("nigger.txt"));
+    println!("{}", file_reader::read_file("testfile.txt"));
 
     let _unimportant: HashMap<char, scanner::Token> = scanner::_scan_token(3);
 }
